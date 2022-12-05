1555ccf7d0be201dd3ba0af13910dbf0f7822109
osdoonhyun <skw1996@naver.com>
2022-12-05 22:42:04 +0900

    [programmers] 연습문제 시간초과 수정

--- programmers/level_1/과일장수.js
+++ programmers/level_1/과일장수.js
@@ -1,11 +1,12 @@
 function solution(k, m, score) {
-  var answer = 0;
-  const length = parseInt(score.length / m, 10);
+  let answer = 0;
+  let idx = m - 1;
+  const length = Math.floor(score.length / m);
   score.sort((a, b) => b - a);
 
   for (let i = 0; i < length; i++) {
-    const arr = score.splice(0, m);
-    answer += arr[m - 1] * m;
+    answer += score[idx] * m;
+    idx += m;
   }
 
   return answer;
