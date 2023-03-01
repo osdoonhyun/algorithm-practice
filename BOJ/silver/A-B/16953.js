7ab68046edac2a5819b6f97d6cd2153e47d2aed6
osdoonhyun <skw1996@naver.com>
2023-03-01 22:27:54 +0900

    [BOJ] silver A->B 왜맞틀?

--- BOJ/silver/A-B/16953.js
+++ BOJ/silver/A-B/16953.js
@@ -5,20 +5,19 @@ const [inputs] = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let [A, B] = inputs.split(' ').map(Number);
 
-console.log('A B', A, B);
-
 function solution(A, B) {
   let cnt = 1;
   while (A !== B) {
     if (A > B) return -1;
 
-    if (B % 2 === 0) B /= 2;
-    else {
-      console.log('B', B);
-      B = [...String(B)];
-      B.pop();
-      B = Number(B.join());
+    if (B % 2 === 0) {
+      B /= 2;
+    } else {
+      const splittedB = String(B).split('');
+      splittedB.pop();
+      B = Number(splittedB.join(''));
     }
+    console.log('A B', A, B);
     cnt++;
   }
   return cnt;
