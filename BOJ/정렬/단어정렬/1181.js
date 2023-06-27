9c7f8528e7ca23481e4dd123774cca9fb2158501
osdoonhyun <skw1996@naver.com>
2023-06-27 16:14:13 +0900

    [BOJ] 정렬 문제 다시 풀기

--- BOJ/정렬/단어정렬/1181.js
+++ BOJ/정렬/단어정렬/1181.js
@@ -3,26 +3,43 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const N = input[0];
+// 2트
+const N = Number(input[0]);
 let arr = [];
 for (let i = 1; i <= N; i++) {
   arr.push(input[i]);
 }
-
 arr = [...new Set(arr)];
 
 arr.sort((a, b) => {
   if (a.length === b.length) {
-    if (a < b) return -1;
-    else if (a > b) return 1;
-    else return 0;
+    return a.localeCompare(b);
+  } else {
+    return a.length - b.length;
   }
-  return a.length - b.length;
 });
+console.log(arr.join('\n'));
+// 1트
+// const N = input[0];
+// let arr = [];
+// for (let i = 1; i <= N; i++) {
+//   arr.push(input[i]);
+// }
 
-let answer = '';
-for (let ele of arr) {
-  answer += ele + '\n';
-}
+// arr = [...new Set(arr)];
 
-console.log(answer);
+// arr.sort((a, b) => {
+//   if (a.length === b.length) {
+//     if (a < b) return -1;
+//     else if (a > b) return 1;
+//     else return 0;
+//   }
+//   return a.length - b.length;
+// });
+
+// let answer = '';
+// for (let ele of arr) {
+//   answer += ele + '\n';
+// }
+
+// console.log(answer);
--- BOJ/정렬/좌표압축/18870.js
+++ BOJ/정렬/좌표압축/18870.js
@@ -3,20 +3,38 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-// const N = input[0];
-const arr = input[1].split(' ').map(Number);
+// 2트
+const N = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
 
-const setArr = [...new Set(arr)];
-const sortedArr = setArr.sort((a, b) => a - b);
-
+let setArr = [...new Set(arr)];
+let sortedSet = setArr.sort((a, b) => a - b);
 let myMap = new Map();
-for (let i = 0; i < sortedArr.length; i++) {
-  myMap.set(sortedArr[i], i);
-}
-console.log(myMap);
-let answer = '';
-for (let ele of arr) {
-  answer += myMap.get(ele) + ' ';
+for (let i = 0; i < N; i++) {
+  myMap.set(sortedSet[i], i);
 }
 
-console.log(answer);
+let answer = [];
+for (const ele of arr) {
+  answer.push(myMap.get(ele));
+}
+console.log(answer.join(' '));
+
+// 1트
+// const N = input[0];
+// const arr = input[1].split(' ').map(Number);
+
+// const setArr = [...new Set(arr)];
+// const sortedArr = setArr.sort((a, b) => a - b);
+
+// let myMap = new Map();
+// for (let i = 0; i < sortedArr.length; i++) {
+//   myMap.set(sortedArr[i], i);
+// }
+// console.log(myMap);
+// let answer = '';
+// for (let ele of arr) {
+//   answer += myMap.get(ele) + ' ';
+// }
+
+// console.log(answer);
