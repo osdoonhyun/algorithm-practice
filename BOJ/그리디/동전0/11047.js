aa1956dcd1cad514a2a236bf41b646d41cb845e2
osdoonhyun <skw1996@naver.com>
2023-06-28 19:50:09 +0900

    [BOJ] 그리디 문제 다시 풀기

--- BOJ/그리디/동전0/11047.js
+++ BOJ/그리디/동전0/11047.js
@@ -3,21 +3,40 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
+// 2트
 let [N, K] = input[0].split(' ').map(Number);
 
-const arr = [];
+let arr = [];
 for (let i = 1; i <= N; i++) {
-  arr.push(input[i]);
+  arr.push(Number(input[i]));
 }
 
-arr.sort((a, b) => b - a);
-let answer = 0;
-
-for (let i = 0; i < N; i++) {
-  answer += parseInt(K / arr[i]);
-  console.log('화폐수', arr[i], answer);
-  K %= arr[i];
-  console.log('남은 돈', K);
+let total = 0;
+for (let i = N - 1; i >= 0; i--) {
+  if (K <= 0) break;
+  if (K / arr[i] >= 1) {
+    let temp = parseInt(K / arr[i]);
+    total += temp;
+    K %= arr[i] * temp;
+  }
 }
+console.log(total);
+// 1트
+// let [N, K] = input[0].split(' ').map(Number);
 
-console.log(answer);
+// const arr = [];
+// for (let i = 1; i <= N; i++) {
+//   arr.push(input[i]);
+// }
+
+// arr.sort((a, b) => b - a);
+// let answer = 0;
+
+// for (let i = 0; i < N; i++) {
+//   answer += parseInt(K / arr[i]);
+//   console.log('화폐수', arr[i], answer);
+//   K %= arr[i];
+//   console.log('남은 돈', K);
+// }
+
+// console.log(answer);
--- BOJ/그리디/신입사원/1946.js
+++ BOJ/그리디/신입사원/1946.js
@@ -4,4 +4,30 @@ const filePath =
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const T = Number(input[0]);
-let N = Number(input[1]);
+
+let answer = [];
+
+let num = 1;
+for (let i = 0; i < T; i++) {
+  let N = Number(input[num]); // 5
+
+  let arr = [];
+
+  for (let j = 0; j < N; j++) {
+    arr.push(input[j + num + 1].split(' ').map(Number));
+  }
+  arr.sort((a, b) => a[0] - b[0]);
+  let minVal = Number.MAX_SAFE_INTEGER;
+  let total = 0;
+  for (let k = 0; k < N; k++) {
+    if (arr[k][1] < minVal) {
+      minVal = arr[k][1];
+      total += 1;
+    }
+  }
+
+  answer.push(total);
+  num += N + 1;
+}
+
+console.log(answer.join('\n'));
