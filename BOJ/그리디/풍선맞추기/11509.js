e44af4711772a1c2cdc813033272410b2048b164
osdoonhyun <skw1996@naver.com>
2023-07-02 17:30:09 +0900

    [BOJ] 그리디 문제 다시 풀기

--- BOJ/그리디/풍선맞추기/11509.js
+++ BOJ/그리디/풍선맞추기/11509.js
@@ -3,23 +3,42 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
+// 2트
 const N = Number(input[0]);
-let H = input[1].split(' ').map(Number);
-
+let arr = input[1].split(' ').map(Number);
 let arrow = Array.from({ length: N }, () => 0);
-let answer = 0;
 
-for (let ele of H) {
+let answer = 0;
+for (let ele of arr) {
   if (arrow[ele] > 0) {
     arrow[ele] -= 1;
     arrow[ele - 1] += 1;
   } else {
-    arrow[ele - 1] += 1;
     answer += 1;
+    arrow[ele - 1] += 1;
   }
 }
 
 console.log(answer);
+
+// 1트
+// const N = Number(input[0]);
+// let H = input[1].split(' ').map(Number);
+
+// let arrow = Array.from({ length: N }, () => 0);
+// let answer = 0;
+
+// for (let ele of H) {
+//   if (arrow[ele] > 0) {
+//     arrow[ele] -= 1;
+//     arrow[ele - 1] += 1;
+//   } else {
+//     arrow[ele - 1] += 1;
+//     answer += 1;
+//   }
+// }
+
+// console.log(answer);
 
 // 내 풀이 -> 시간초과
 // let cnt = 0;
