f4519ed9ffc1c8ad486c7ba25d56422b9143a670
osdoonhyun <skw1996@naver.com>
2023-07-05 17:00:29 +0900

    [BOJ] 이진탐색 문제 다시 풀기

--- BOJ/이진탐색/병사배치하기/18353.js
+++ BOJ/이진탐색/병사배치하기/18353.js
@@ -4,41 +4,48 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
+// 2트
 const N = Number(input[0]);
 let arr = input[1].split(' ').map(Number);
 
 arr.reverse();
 
-let answer = [0];
+let k = [0];
+for (const ele of arr) {
+  if (k[k.length - 1] < ele) {
+    k.push(ele);
+  } else {
+    let index = lowerBound(k, ele, 0, k.length);
+    k[index] = ele;
+  }
+}
+
+console.log(N - k.length + 1);
+
+// 1트
+// const N = Number(input[0]);
+// let arr = input[1].split(' ').map(Number);
+
+// arr.reverse();
+
+// let answer = [0];
 
 // function lowerBound(arr, target, start, end) {
 //   while (start < end) {
 //     let mid = parseInt((start + end) / 2);
-//     if (target <= arr[mid]) {
-//       end = mid;
-//     } else {
-//       start = mid + 1;
-//     }
+//     if (arr[mid] >= target) end = mid;
+//     else start = mid + 1;
 //   }
 //   return end;
 // }
 
-function lowerBound(arr, target, start, end) {
-  while (start < end) {
-    let mid = parseInt((start + end) / 2);
-    if (arr[mid] >= target) end = mid;
-    else start = mid + 1;
-  }
-  return end;
-}
+// for (const ele of arr) {
+//   if (answer[answer.length - 1] < ele) {
+//     answer.push(ele);
+//   } else {
+//     let index = lowerBound(answer, ele, 0, answer.length);
+//     answer[index] = ele;
+//   }
+// }
 
-for (const ele of arr) {
-  if (answer[answer.length - 1] < ele) {
-    answer.push(ele);
-  } else {
-    let index = lowerBound(answer, ele, 0, answer.length);
-    answer[index] = ele;
-  }
-}
-
-console.log(N - answer.length + 1);
+// console.log(N - answer.length + 1);
--- BOJ/이진탐색/숫자카드2/10816.js
+++ BOJ/이진탐색/숫자카드2/10816.js
@@ -8,14 +8,16 @@ const arrN = input[1].split(' ').map(Number);
 const M = Number(input[2]);
 const arrM = input[3].split(' ').map(Number);
 
+// 2트
 function lowerBound(arr, target, start, end) {
   while (start < end) {
     let mid = parseInt((start + end) / 2);
-    if (arr[mid] >= target) end = mid;
+    if (target <= arr[mid]) end = mid;
     else start = mid + 1;
   }
   return end;
 }
+
 function upperBound(arr, target, start, end) {
   while (start < end) {
     let mid = parseInt((start + end) / 2);
@@ -25,9 +27,9 @@ function upperBound(arr, target, start, end) {
   return end;
 }
 
-function countByRange(arr, LeftValue, RightValue) {
-  let rightIndex = upperBound(arr, RightValue, 0, arr.length);
-  let leftIndex = lowerBound(arr, LeftValue, 0, arr.length);
+function countBound(arr, left, rigth) {
+  let rightIndex = upperBound(arr, rigth, 0, arr.length);
+  let leftIndex = lowerBound(arr, left, 0, arr.length);
   return rightIndex - leftIndex;
 }
 
@@ -35,8 +37,43 @@ arrN.sort((a, b) => a - b);
 
 let answer = [];
 for (const ele of arrM) {
-  let cnt = countByRange(arrN, ele, ele);
+  let cnt = countBound(arrN, ele, ele);
+
   answer.push(cnt);
 }
 
 console.log(answer.join(' '));
+
+// 1트
+// function lowerBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = parseInt((start + end) / 2);
+//     if (arr[mid] >= target) end = mid;
+//     else start = mid + 1;
+//   }
+//   return end;
+// }
+// function upperBound(arr, target, start, end) {
+//   while (start < end) {
+//     let mid = parseInt((start + end) / 2);
+//     if (target < arr[mid]) end = mid;
+//     else start = mid + 1;
+//   }
+//   return end;
+// }
+
+// function countByRange(arr, LeftValue, RightValue) {
+//   let rightIndex = upperBound(arr, RightValue, 0, arr.length);
+//   let leftIndex = lowerBound(arr, LeftValue, 0, arr.length);
+//   return rightIndex - leftIndex;
+// }
+
+// arrN.sort((a, b) => a - b);
+
+// let answer = [];
+// for (const ele of arrM) {
+//   let cnt = countByRange(arrN, ele, ele);
+//   answer.push(cnt);
+// }
+
+// console.log(answer.join(' '));
