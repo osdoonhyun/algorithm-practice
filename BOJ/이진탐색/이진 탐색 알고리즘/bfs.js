e3fb60fdf7a5ba78c993ef5300fb5b9681b266eb
osdoonhyun <skw1996@naver.com>
2024-02-15 22:46:05 +0900

    [이진탐색] BFS

---
+++ BOJ/이진탐색/이진 탐색 알고리즘/bfs.js
@@ -0,0 +1,39 @@
+/**
+ * 너비 우선 탐색(breadth-first search: BFS)
+ * 맹목적 탐색방법의 하나로 시작 정점을 방문한 후 시작 정점에 인접한 모든 정점들을 우선 방문하는 방법이다. 더 이상 방문하지 않은 정점이 없을 때까지 방문하지 않은 모든 정점들에 대해서도 너비 우선 검색을 적용한다. OPEN List 는 큐를 사용해야만 레벨 순서대로 접근이 가능하다.
+ * (출처 :https://ko.wikipedia.org/wiki/너비_우선_탐색)
+ */
+
+const graph = {
+  A: ['B', 'C'],
+  B: ['A', 'D'],
+  C: ['A', 'G', 'H', 'I'],
+  D: ['B', 'E', 'F'],
+  E: ['D'],
+  F: ['D'],
+  G: ['C'],
+  H: ['C'],
+  I: ['C', 'J'],
+  J: ['I'],
+};
+
+const BFS = (graph, startNode) => {
+  const visited = []; // 탐색을 마친 노드들
+  let needVisit = []; // 탐색해야할 노드들
+
+  needVisit.push(startNode); // 노드 탐색 시작
+
+  while (needVisit.length !== 0) {
+    // 탐색해야할 노드가 남아있다면
+    const node = needVisit.shift(); // queue이기 때문에 선입선출, shift()를 사용한다.
+    if (!visited.includes(node)) {
+      // 해당 노드가 탐색된 적 없다면
+      visited.push(node);
+      needVisit = [...needVisit, ...graph[node]];
+    }
+  }
+  return visited;
+};
+
+console.log(BFS(graph, 'A'));
+// ["A", "B", "C", "D", "G", "H", "I", "E", "F", "J"]
